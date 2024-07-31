949a6b185f4c28e1410684b0d0116bf740c2f42d
Jamal Ibanni <60659076+sialorama@users.noreply.github.com>
2024-07-31 13:47:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,46 +6,43 @@ from apikey import apikey
 from streamlit_carousel import carousel
 
 # Initialize your image generation client
-client=OpenAI(api_key=apikey)
+client = OpenAI(api_key=apikey)
 
-single_img=dict(
-            title="",
-            text="",
-            interval=None,
-            img="",
-
-        )
+single_img = dict(
+    title="",
+    text="",
+    interval=None,
+    img="",
+)
 
 def generate_images(image_description, number_of_images):
-    image_galery = []
-    for i in range (number_of_images):
+    image_gallery = []
+    for i in range(number_of_images):
         img_response = client.images.generate(
-                        model = "dall-e-3",
-                        prompt = image_description,
-                        n = 1,
-                        size = "1024x1024"
-                        )
-        image_url = img_response.data[0].url
-        new_image=single_img.copy()
-        new_image["title"]=f"Image(i+1)"
-        new_image["texte"]=image_description
-        new_image["img"]=image_url
-        image_galery.append(new_image)
+            model="dall-e-3",
+            prompt=image_description,
+            n=1,
+            size="1024x1024"
+        )
+        image_url = img_response['data'][0]['url']
+        new_image = single_img.copy()
+        new_image["title"] = f"Image {i+1}"
+        new_image["text"] = image_description
+        new_image["img"] = image_url
+        image_gallery.append(new_image)
+    return image_gallery
 
-    return image_galery
-
-st.set_page_config(page_title="Générateur d'images Dalle", page_icon=":camera:", layout="wide")
+st.set_page_config(page_title="Générateur d'images Dall-E", page_icon=":camera:", layout="wide")
 
 # Create title
 st.title("Générateur d'images avec Dall-E-3")
 
 # Create a subheader
-st.subheader("Réalisé avec le puissant model de génération d'images API-Dall-E")
-image_description = st.text_input("Entrer une description de l'image que vous souhaitez générer",  "Brest by night")
-number_of_images = st.number_input("Séléctionnez le nombre d'images que vous vouez générer", min_value=1, max_value=10, value=1)
+st.subheader("Réalisé avec le puissant modèle de génération d'images API-Dall-E")
+image_description = st.text_input("Entrer une description de l'image que vous souhaitez générer", "Brest by night")
+number_of_images = st.number_input("Sélectionnez le nombre d'images que vous voulez générer", min_value=1, max_value=10, value=1)
 
 # Create a submit button
 if st.button("Générer"):
-    generate_images = generate_images(image_description, number_of_images)
-
-    carousel(items=generate_images, width=1)
+    generated_images = generate_images(image_description, number_of_images)
+    carousel(items=generated_images, width=1)
